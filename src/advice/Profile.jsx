aef4cd26bf8408b6098839ee9d51c574fcd0d364
Divyansh Pandey <100780218+divuInevitable@users.noreply.github.com>
2022-07-09 18:20:28 +0530

    Fixed spelling mistakes

--- src/advice/Profile.jsx
+++ src/advice/Profile.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 function Profile(props) {
-    const { childe, size } = props
+    const { children, size } = props
 
     if(size === "small") 
-        return <div className='h-8 w-8 bg-red-600 rounded-full'>{childe}</div>
+        return <div className='h-8 w-8 bg-red-600 rounded-full'>{children}</div>
     if(size === "medium")
-        return <div className='w-[18.75rem] h-[13.063rem] rounded-3xl bg-slate-700'>{childe}</div>
+        return <div className='w-[18.75rem] h-[13.063rem] rounded-3xl bg-slate-700'>{children}</div>
     if(size === "large") 
-        return <div className='h-[12.625rem] w-[12.625rem] bg-yellow-600 rounded-3xl'>{childe}</div>
+        return <div className='h-[12.625rem] w-[12.625rem] bg-yellow-600 rounded-3xl'>{children}</div>
     if(size === "square") 
-        return <div className='h-[7.5rem] w-[7.5rem] bg-green-600 rounded-3xl'>{childe}</div>
+        return <div className='h-[7.5rem] w-[7.5rem] bg-green-600 rounded-3xl'>{children}</div>
 
   return (
-    <div>{childe}</div>
+    <div>{children}</div>
   )
 }
 
